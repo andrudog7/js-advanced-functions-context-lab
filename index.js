df64849eacc8beee8969e7771a1512b612064e67
--add <andrewksmoker@gmail.com>
2021-02-01 16:17:13 -0500

    commit tree 84315f0a92086ea39eddb973a9722064fb2296ae

--- index.js
+++ index.js
@@ -9,6 +9,51 @@
  for you to use if you need it!
  */
 
+function createEmployeeRecord(arr){
+    const employee = {}
+    employee.firstName = arr[0]
+    employee.familyName = arr[1]
+    employee.title = arr[2]
+    employee.payPerHour = arr[3]
+    employee.timeInEvents = []
+    employee.timeOutEvents = []
+    return employee
+}
+
+function createEmployeeRecords(src) {
+    const employees = []
+    src.map(e => employees.push(createEmployeeRecord(e)))
+    return employees
+}
+
+function createTimeInEvent(date) {
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(date.slice(-4)),
+        date: date.split(" ")[0],
+    })
+    return this
+}
+
+function createTimeOutEvent(date) {
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(date.slice(-4)),
+        date: date.split(" ")[0],
+    })
+    return this
+}
+
+function hoursWorkedOnDate(date) {
+    const clockIn = this.timeInEvents.find(e => e.date === date).hour
+    const clockOut = this.timeOutEvents.find(e => e.date === date).hour
+    return (parseInt(clockOut) - parseInt(clockIn)) / 100
+}
+
+function wagesEarnedOnDate(date) {
+    return hoursWorkedOnDate.call(this, date) * this.payPerHour
+}
+
 let allWagesFor = function () {
     let eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
@@ -19,4 +64,14 @@ let allWagesFor = function () {
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
+}
+
+function calculatePayroll(src) {
+    return src.reduce(
+        (total, emp) => total + allWagesFor.call(emp), 0
+    )
+}
+
+function findEmployeeByFirstName(src, firstName) {
+    return src.find(employee => employee.firstName === firstName)
 }
